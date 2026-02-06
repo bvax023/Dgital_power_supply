ac498cf4000373d8ba1bc6f71aab0d9029e4a6d8
bvax023 <bvax023@gmail.com>
2026-02-06 17:42:58 +0200

    Update sketch_jan24a.ino

--- sketch_jan24a/sketch_jan24a.ino
+++ sketch_jan24a/sketch_jan24a.ino
@@ -23,13 +23,13 @@ Encoder enc(CLK_PIN, DT_PIN, SW_PIN);
 int setV = 1200; // Уставка Напряжения (12.00 В)
 int setI = 100;  // Уставка Тока (1.00 А)
 
-float realV = 0;   // Измеренное напряжение
-float realI = 0;   // Измеренный ток
-float realP = 0;  // Мощность
+float readV = 0;   // Измеренное напряжение
+float readI = 0;   // Измеренный ток
+float readP = 0;  // Мощность
 int tempC = 35;  // Заглушка температуры
 
 int cursorStep = 0; // 0 = Главный экран, 1,2,3,4 = Редактирование разряда (1-десятки, 2-единицы, 3-десятые, 4-сотые)
-bool editVoltage = true; // true = Set V, false = Set I
+bool setEdit = true; // true = Set V, false = Set I
 
 // --- Таймеры ---
 uint32_t blinkTimer = 0; // Таймер для мигания активным разрядом при установке Set V, Set I
@@ -44,9 +44,10 @@ const uint8_t dPos[] = {7, 8, 10, 11}; // [0]=десятки, [1]=единицы
 const int addValue[] = {0, 1000, 100, 10, 1}; // [0]=пусто, [1]=10В, [2]=1В, [3]=0.1В, [4]=0.01В
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   lcd.init(); // Инициализация экрана
   lcd.backlight();  
+  Wire.setClock(400000);
   enc.setType(TYPE2); // Тип энкодера
 
   // Инициализация железа (раскомментировать при подключении)
@@ -56,7 +57,7 @@ void setup() {
   ads.setGain(GAIN_SIXTEEN);
   ads.setDataRate(RATE_ADS1115_8SPS);
   
-  updateHardware(); // Применяем стартовые значения   
+  setDAC(); // Применяем стартовые значения   
   lcd.clear();
   renderAll(); // Первая полная отрисовка экрана
 }
@@ -66,23 +67,23 @@ void loop() {
   enc.tick();
   
   // 2. Обработка енкодера (клики, повороты)
-  handleControl();
+  encoderSet();
 
   // 3. Анимация курсора (мигание)
   digitBlinking();
 
   // 4. Работа с датчиками (чтение / расчет мощности)
-  handleSensors();
+  readADS();
 }
 
 // ================= ФУНКЦИИ ЛОГИКИ =================
 
 // --- Обработка управления (Энкодер) ---
-void handleControl() {  
+void encoderSet() {  
   // Клик (Вход в Set V / Переход по разрядам)
   if (enc.isClick()) {
     if (cursorStep == 0) { // если мы на главыном экране вход в Set V    
-      editVoltage = true;
+      setEdit = true;
       //cursorStep = 1;
       cursorStep = 2; // Начинаем установку с единиц
     } else { // Мы в режиме установки, листаем разряд           
@@ -97,7 +98,7 @@ void handleControl() {
   // Длинное нажатие (Смена режима V/I или Выход)
   if (enc.isHolded()) {
     if (cursorStep == 0) { // если мы на главыном экране вход в Set I       
-      editVoltage = false;
+      setEdit = false;
       //cursorStep = 1;
       cursorStep = 2; // Начинаем установку с единиц
     } else { // Если мы в режиме Set V, Set I по длинному нажатию выходим       
@@ -110,9 +111,9 @@ void handleControl() {
   if (enc.isTurn() && cursorStep > 0) {
     // Берем шаг изменения из глобального массива (1000, 100, 10 или 1)
     int delta = addValue[cursorStep];
-    if (editVoltage) { // Set V      
+    if (setEdit) { // Set V      
       if (enc.isRight()) setV += delta; else setV -= delta;
-      setV = constrain(setV, 0, 2200); // Лимит 0..30.00В
+      setV = constrain(setV, 0, 2200); // Лимит 0..22.00В
     } else { // Set I      
       if (enc.isRight()) setI += delta; else setI -= delta;
       setI = constrain(setI, 0, 1000); // Лимит 0..10.00А
@@ -121,7 +122,7 @@ void handleControl() {
     blinkTimer = millis(); // Сбрасываем таймер миганием разряда
     renderAll(); // Перерисовываем экран
 
-    updateHardware();     // Обновляем ЦАПы  
+    setDAC();     // Обновляем ЦАПы  
   }
 }
 
@@ -143,7 +144,7 @@ void updateBlinkDigit() {
     // Просто перерисовываем число целиком.    
     lcd.setCursor(7, 1); // Курсор ставим на начало числа (позиция 7 для "Set >V:12.34")
     
-    if (editVoltage) printFormatted(setV); 
+    if (setEdit) printFormatted(setV); 
     else             printFormatted(setI);
     
   } else { // мигаем автивным разрядом   
@@ -170,19 +171,20 @@ void printFormatted(int val) {
 
 // --- Отрисовка всего экрана ---
 void renderAll() {  
-  lcd.setCursor(0, 0); // realV
-  lcd.print(realV, 3); lcd.print(F("V")); 
-  lcd.setCursor(10, 0); // realI
-  lcd.print(realI, 3); lcd.print(F("A"));
+  lcd.setCursor(0, 0); // readV
+  lcd.print(readV, 3); lcd.print(F("V")); 
+  lcd.setCursor(10, 0); // readI
+  lcd.print(readI, 3); lcd.print(F("A"));
 
-  // Вторая строка  
-  if (cursorStep == 0) { // главный экран, во второй строке показываем Мощность (W) и Температуру (C)   
-    lcd.setCursor(0, 1); 
-    // int wattsX10 = realP / 1000; // Вывод ватт    
+  // Вторая строка
+  lcd.setCursor(0, 1);
+  if (cursorStep == 0) { // главный экран, во второй строке показываем Мощность (W) и Температуру (C)    
+    // int wattsX10 = readP / 1000; // Вывод ватт    
     // lcd.print(wattsX10 / 10); // Целые 
     // lcd.print('.');
-    // lcd.print(wattsX10 % 10); // Десятые 
-    printFormatted(realP / 100);
+    // lcd.print(wattsX10 % 10); // Десятые     
+    //printFormatted(readP / 100);
+    lcd.print(readP, 3);
     lcd.print(F("W        "));   // Чистим место
 
     // Температура справа
@@ -191,13 +193,12 @@ void renderAll() {
     lcd.print(F("C "));
     
   } else {
-    lcd.setCursor(0, 1); 
     // === РЕЖИМ НАСТРОЙКИ ===
-    if (editVoltage) lcd.print(F("Set >V:"));
+    if (setEdit) lcd.print(F("Set >V:"));
     else             lcd.print(F("Set >I:"));
     
     // Печатаем значение уставки
-    if (editVoltage) printFormatted(setV);
+    if (setEdit) printFormatted(setV);
     else             printFormatted(setI);
     
     lcd.print(F("    ")); // Чистим хвост строки
@@ -205,7 +206,7 @@ void renderAll() {
   }
 }
 
-  void handleSensors() {
+  void readADS() {
     static uint8_t adcStep = 0;
     static uint32_t adcTimer = 0;
     
@@ -215,11 +216,14 @@ void renderAll() {
     // Диапазон +/- 256 мВ делим на 32768 шагов
     const float ADC_STEP_MV = 0.0078125; 
 
-    // 2. Коэффициент делителя напряжения (160к + 1к)
+    // Коэффициент делителя напряжения (160к + 1к)
     const float V_DIVIDER = 0.161;
 
-    // 3. Коэффициент для делителя для тока  
-    const float I_RATIO = 0.312; 
+    // Калибровочный коэфициент для напряжения
+    const float corrV = 0.9975;
+
+    // Калибровочный коэфициент для тока 
+    const float corrI = 1.281; 
 
     switch (adcStep) {
       // --- 1. ЗАМЕР НАПРЯЖЕНИЯ (A0-A1) ---
@@ -234,17 +238,13 @@ void renderAll() {
           int16_t rawV = ads.getLastConversionResults();
           
           // Шаг 1: Считаем напряжение на ножке АЦП (в мВ)
-          float pinMV = rawV * ADC_STEP_MV;
+          float pinV = rawV * ADC_STEP_MV;
           
           // Шаг 2: Умножаем на делитель (получаем реальные мВ на выходе БП)
-          realV = pinMV * V_DIVIDER; // например 12000.0
+          readV = pinV * V_DIVIDER; // например 12000.0
+          readV = readV*corrV;      
           
-          // Шаг 3: Переводим в формат "сотые вольта" для int (12000 / 10 = 1200)
-          //realV = (int)(totalMV / 10.0);    
-          //Serial.print("realV: ");
-          //Serial.println(realV, 3); // Выведет ток с 4 знаками после запятой     
-          
-          if (realV < 0) realV = 0;
+          if (readV < 0) readV = 0;
           adcStep = 2; 
           renderAll();
         }
@@ -262,22 +262,17 @@ void renderAll() {
           int16_t rawI = ads.getLastConversionResults();
           
           // Шаг 1: Считаем напряжение, которое дошло до АЦП (в мВ)
-          float pinMV = abs(rawI) * ADC_STEP_MV;
-          //pinMV/10.0;
+          float pinI = abs(rawI) * ADC_STEP_MV;
+      
           // Шаг 2: Переводим мВ в Амперы по вашей пропорции (78мВ = 10А)
-          realI = pinMV/10.0;
+          readI = pinI*corrI;
+          readI = readI/10.0;
 
-          //Serial.print("realI: ");
-          //Serial.println(realI, 3); // Выведет ток с 4 знаками после запятой 
-
-          
-          
-          // Шаг 3: Переводим в формат "сотые ампера" для int (1.00А -> 100)
-          // Но! У вас 10.00А это 1000. Значит умножаем на 100.
-          //realI = pinMV/10.0;                
+          //Serial.print("readI: ");
+          //Serial.println(readI, 3); // Выведет ток с 4 знаками после запятой                     
           
           // Мощность
-          realP = (long)realV * realI;       
+          //readP = readV * readI;       
          
           adcStep = 0; 
           renderAll();
@@ -287,8 +282,8 @@ void renderAll() {
   }
 
   // --- Обновление железа (ЦАП) ---
-  void updateHardware() {
+  void setDAC() {
     // Тут будет код для MCP4725
-     dacV.setVoltage(map(setV, 0, 2230, 0, 4095), false);
+     dacV.setVoltage(map(setV, 0, 2230, 0, 4095), false); // 2230 для калибровки
      dacI.setVoltage(map(setI, 0, 1000, 0, 4095), false);
   }
